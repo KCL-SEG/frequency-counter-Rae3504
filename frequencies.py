4a6064d2cc94f97e823a45023c4086e40840076d
Raees Haiy <r.haiy04@gmail.com>
2023-10-05 22:15:27 +0100

    Create frequency counter

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+	frequencies = {}
+
+	for item in items:
+		name = str(item)
+		frequencies[name] = frequencies.get(name, 0) + 1
+
+	return frequencies
+
+print(frequencies([1,1,2]))
